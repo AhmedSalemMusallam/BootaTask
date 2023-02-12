70645da0b3512d51345b8d92e151e65252ba32ab
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-02-12 18:25:39 +0200

    Project Creation - Commit

---
+++ BostaTask/BostaTask/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BostaTask
+//
+//  Created by Ahmed Salem on 12/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
